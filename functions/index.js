190cd0dc0b0844bae96ad8dfd8c3cce727a5b72e
Nicole Vukadinova <nicole_mail@abv.bg>
2018-11-24 16:42:23 +0000

    fill in information on website and implement adding an RB account when finishing sign up

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const util = require('util');
 const authy = require('authy');
+const request = require('request');
 
 const AUTHY_API_KEY = 'KiPj41iB83L66Q90y9CQh8feaB3pxBkx';
 
@@ -17,4 +18,23 @@ exports.sendMobileCode = functions.https.onCall((data, context) => {
 	const client = authy(AUTHY_API_KEY);
 	client.verifyAsync = util.promisify(client.phones().verification_start);
 	return client.verifyAsync(data.phoneNumber, data.countryCode, { via: 'sms', code_length: 4 });
+});
+
+exports.finishSignUp = functions.https.onCall((data, context) => {
+	var enduser_id;
+    //create RB account and store enduser_id to FireBase
+    request.post({url:'https://play.railsbank.com/v1/customer/endusers', formData: {
+        person: {
+            name: data.name,
+            telephone: data.phoneNumber
+        }
+    }}, function (error, response, body) {
+        if (err) {
+            return console.error('failed:', err);
+        }
+        
+        enduser_id = body;
+    });
+    
+    console.log(enduser_id);
 });
